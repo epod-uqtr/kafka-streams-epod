b490ba2cd8a117e5559babda443d7f719b042ca6
zinnourlahcene <zinnour@uqtr.ca>
2020-05-03 21:12:44 -0400

    init

---
+++ src/main/java/ca/uqtr/kafkastreamsepod/KafkaStreamsEpodApplication.java
@@ -0,0 +1,13 @@
+package ca.uqtr.kafkastreamsepod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsEpodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamsEpodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/uqtr/kafkastreamsepod/KafkaStreamsEpodApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.uqtr.kafkastreamsepod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsEpodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
